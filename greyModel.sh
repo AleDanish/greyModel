d7f74dfda9edb47973ef1aba7bce4825273cb140
Alessandro <alessandrozanni.dev@gmail.com>
2016-03-02 18:08:26 +0100

    jars moved

--- greyModel.sh
+++ greyModel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-java -cp .:Jama-1.0.2.jar:py4j0.9.1.jar py4j/Grey py4j/IFilter &
+java -cp .:py4j/Jama-1.0.2.jar:py4j/py4j0.9.1.jar py4j/Grey py4j/IFilter &
 sleep 1
 python greyModel.py
