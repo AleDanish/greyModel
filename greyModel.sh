a0a3a617de1bb3a47b2343f5583337837876dedf
Alessandro <alessandrozanni.dev@gmail.com>
2016-03-04 18:03:57 +0100

    greyModel

--- greyModel.sh
+++ greyModel.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-ip=$(lsof -i:25333 -t)
-kill -9 $ip
+kill -9 $(lsof -i:25333 -t)
 
 javac -cp py4j/Jama-1.0.2.jar:py4j/py4j0.9.1.jar py4j/Grey.java py4j/IFilter.java
 java -cp .:py4j/Jama-1.0.2.jar:py4j/py4j0.9.1.jar py4j/Grey py4j/IFilter &
