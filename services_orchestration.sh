2493a195bc7f4e8e9bf51a0ef5f20b44db0ea92a
AleDanish <superalle87@gmail.com>
2016-03-18 10:05:27 +0000

    pip install py4j

--- services_orchestration.sh
+++ services_orchestration.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 kill -9 $(lsof -i:25333 -t)
+sudo pip install py4j
 
 javac -cp py4j/Jama-1.0.2.jar:py4j/py4j0.9.1.jar py4j/Grey.java py4j/IFilter.java
 java -cp .:py4j/Jama-1.0.2.jar:py4j/py4j0.9.1.jar py4j/Grey py4j/IFilter &
